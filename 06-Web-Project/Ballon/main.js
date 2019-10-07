1c60d5fa07d436a623b05cde56930398fd104aef
Jake Overall <joverall22@gmail.com>
2019-10-07 17:14:19 -0600

    adds balloon pump

--- 06-Web-Project/Ballon/main.js
+++ 06-Web-Project/Ballon/main.js
@@ -1,7 +1,3 @@
-// BUTTONS
-let startButton = document.getElementById('start-button')
-let inflateButton = document.getElementById('inflate-button')
-
 // #region GAME LOGIC AND DATA
 
 // DATA 
@@ -18,8 +14,8 @@ let timeRemaining = 0
 let currentPlayer = {}
 
 function startGame() {
-  startButton.setAttribute("disabled", "true")
-  inflateButton.removeAttribute("disabled")
+  document.getElementById("game-controls").classList.remove("hidden")
+  document.getElementById("main-controls").classList.add("hidden")
   startClock()
   setTimeout(stopGame, gameLength)
 }
@@ -74,8 +70,8 @@ function draw() {
 function stopGame() {
   console.log("the game is over")
 
-  inflateButton.setAttribute("disabled", "true")
-  startButton.removeAttribute("disabled")
+  document.getElementById("main-controls").classList.remove("hidden")
+  document.getElementById("game-controls").classList.add("hidden")
 
   clickCount = 0
   height = 120
